d42d66c4e5b4b83a49411adb4f5f45edc8ec7e27
nicoquagliata <nicoquagliata@gmail.com>
2019-11-24 19:34:55 -0300

    updating Task module

--- models/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const Task = new Schema ({
-    title: String,
-    description: String
-});
-
-module.exports = mongoose.model('Task', Task);
