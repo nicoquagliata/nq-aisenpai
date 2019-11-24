d01ab357e18654772ed7b57e1f5f71c6996ffab6
nicoquagliata <nicoquagliata@gmail.com>
2019-11-24 13:45:36 -0300

    added vue and bootstrap - working

---
+++ app/components/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-light">
+            <a href="/" class="navbar-brand">HOME</a>
+        </nav>
+        <div class="container">
+            <div class="row pt-5">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" placeholder="Insert a Task" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <textarea name="description" id="" cols="30" rows="10" class="form-control" placeholder="Insert your description"></textarea>
+                                </div>
+                                <button class="btn btn-primary btn-block">Add Task</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- app/index.js
+++ app/index.js
@@ -1,1 +1,6 @@
-alert ('Vuejs goes here')
+import Vue from 'vue';
+import App from './components/App.vue';
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app');
